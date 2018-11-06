b903b01318d55e872b42503b80e15807a388cf10
wangjunemse <wangjuncn0909@gmail.com>
2018-11-06 23:52:09 +0800

    home page

---
+++ js/popup.js
@@ -0,0 +1,12 @@
+// JavaScript
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.carousel');
+sr.reveal('.row');
+sr.reveal('.solutions');
+sr.reveal('.benefits');
+sr.reveal('.text');
+sr.reveal('.roadmap');
+sr.reveal('.map');
+sr.reveal('.advisor-page');
+
+// sr.reveal('about-page');
